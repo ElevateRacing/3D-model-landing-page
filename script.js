ad05cfd67ad2d0e24817d63846788103d6e75ac0
ElevateRacing <m.michalczyk@sky-ride.com>
2025-02-27 14:57:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.getElementById('container');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xffffff);
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
